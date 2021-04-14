f6b8f1dd77f7bc849796e83027ec0e7ba6548c4c
Antwan Grant <twanjobs68@gmail.com>
2021-04-14 15:28:52 -0400

    added homepage functionality

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function plan () {
+    alert("Dertermine who will go first by heads or tails!!");
+    alert("ClIck corresponding button below !")
+}
+plan();
+
+
+function inThis(sqr) {
+    alert("You put a letter here!!");
+}
