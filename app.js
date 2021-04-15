604e50ed92d42ace63a4df837589632f41cbebd9
Antwan Grant <twanjobs68@gmail.com>
2021-04-14 22:03:15 -0400

    added function to check for blank box in game when selected*/

--- app.js
+++ app.js
@@ -10,20 +10,32 @@
 
 //*function added to alert user when a box has been clicked on*/
 //* change function to check if O is in entered in square or and switch to x for next player turns*/
-var thePlayer= "O";
+var thePlayer= "o";
+console.log(thePlayer);
 function inthe(sqr) {
+
     /*alert("You put a letter here!!");*/
-    sqr.innerText = thePlayer;
-    if(thePlayer == "O") {
-        thePlayer = "X";
-    }
-    //*add check for O is not in box. means an x is played. play x and change for next user to have 'O' when played*//
-    else{
-        thePlayer == "O";
-    }
+    /*added if statement to prevent changing letters once entered in box*/
+    //*learned about ternary operator.  applying to this if statemnt*/
+    /*This if statement checks to see if the square is not empty so that play may stop */
+    /*so next player can take a Take a turn*/
+  /* if(sqr.innertText != "") return;*/
+   sqr.innerText = thePlayer;
+   //*add check for whats currently in the box clcked. if its an o then switch to x for next user to play */
+   /*else make it an O */
+    // if(thePlayer == "o") {
+    //     thePlayer = "x";
+      
+    // }/*simplified if statement using ternary opetator*/
+    thePlayer == "o" ? thePlayer = "x" : thePlayer = "o";
+    
+    // else{
+    //     thePlayer == "o";
+    // }
 }
-function plyrs () {
-var plyr1 = window.prompt("Player 1-Please Enter your name: "); alert("Hello: " + plyr1);
-var plyr2 = window.prompt("Player 2-Please Enter your name: "); alert("Hello: " + plyr2);
-}
-plyrs();
+//*window prompt ask for players names and says hello at beginning of game but does it more than once-->
+// function plyrs () {
+// var plyr1 = window.prompt("Player 1-Please Enter your name: "); alert("Hello: " + plyr1);
+// var plyr2 = window.prompt("Player 2-Please Enter your name: "); alert("Hello: " + plyr2);
+// }
+// plyrs();
