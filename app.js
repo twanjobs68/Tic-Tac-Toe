4278588c165f86a0b87b918322cc2e07eae650d5
Antwan Grant <twanjobs68@gmail.com>
2021-04-15 21:58:22 -0400

    simplified if statements and for loop process.  In Loop.

--- app.js
+++ app.js
@@ -10,10 +10,8 @@
 
 //*function added to alert user when a box has been clicked on*/
 //* change function to check if O is in entered in square or and switch to x for next player turns*/
-var thePlayer= "O";
-function inthe(sqr) {
-    
-    /*alert("You put a letter here!!");*/
+
+   
     /*added if statement to prevent changing letters once entered in box*/
     //*learned about ternary operator.  applying to this if statemnt*/
     /*This if statement checks to see if the square is not empty so that play may stop -DOES NOT WORK*/
@@ -21,25 +19,26 @@ function inthe(sqr) {
     // if(sqr.innertText != " "){
    
 //    }
-              
-   sqr.innerText = thePlayer; /*set square value to current player value of thePlayer -initally "O"*/
-   console.log(sqr.innerText);
-  /*if the square is not blank*/
-    console.log("hello");
-    
-    
-   //*add check for whats currently in the box clcked. if its an o then switch to x for next user to play */
+  var thePlayer= "O"; 
+  var win = false;
+  function inthe(sqr) {
+           
+            if( sqr.innerText != "" || win) {
+                return;
+            }
+         sqr.innerText = thePlayer; /*set square value to current player value of thePlayer -initally "O"*/
+           /*if the square is not blank*/
+         
+    //*add check for whats currently in the box clcked. if its an o then switch to x for next user to play */
    /*else make it an O */
     // if(thePlayer == "O") {
     //     thePlayer = "X";
-      
     // }/*simplified if statement using ternary opetator-if current player 'O', changed to 'X' otherwise set to 'O'*/
     thePlayer == "O" ? thePlayer = "X" : thePlayer = "O";
-    
-    fndWinner();
-             }
- 
-            
+
+    fndThree(); /*function that checks for 3 x'x or 3 o's in a row or colunmn*/
+     }
+             
     // else{
     //     thePlayer == "O";
     // }
@@ -49,78 +48,73 @@ function inthe(sqr) {
      //var chckFirstSqr = document.getElementById("0_0");
         // alert(chckFirstSqr.innerText);
         
-     function fndWinner(){
+     function fndThree(){
          //functionality added to count through each row on board till 3 of same letters(innerText) found in the row
-            for(letterCnt = 0; letterCnt <= 2; letterCnt++){
-                 var chckFirstSqr = document.getElementById("0_" + letterCnt).innerText;
-                 var chckSecndSqr = document.getElementById("1_" + letterCnt).innerText; 
-                 var chckThirdSqr = document.getElementById("2_" + letterCnt).innerText;
-            }
-                 for(letterCnt = 0; letterCnt <= 2; letterCnt++){
-                     var chckFirstSqr = document.getElementById(letterCnt + "0_").innerText;
-                    var chckSecndSqr = document.getElementById(letterCnt + "1_").innerText; 
-                     var chckThirdSqr = document.getElementById(letterCnt + "2_").innerText;
-                 }
-                
-
-                 if(chckFirstSqr == " ") {
-                     continue;
-                    }
-
-    /* make sure first box not empty before checking for winner- Doesn't work like expected*/
-          //  if(chckFirstSqr == " "){
-
-         //function to find 3 in a row by checking locating square,checking letter in square
-        //and comparing 3 squares to see if equal and announce winner 
-                if(chckFirstSqr == chckSecndSqr && chckFirstSqr == chckThirdSqr){
-                    /*Extra.  Instead of X wins, display player name and letter in parenthes "is Winner"*/
-                    alert(chckFirstSqr + "s WIN!!!!!");
-                     // alert("WINNER!!!");
-                }
-           
-           //check letters diagnolly left to right then right to left for winner  
-           var chckFirstDiaLr = document.getElementById("0_0").innerText;
-           var chckSecndDiaLr = document.getElementById("1_1").innerText; 
-           var chckThirdDiaLr = document.getElementById("2_2").innerText;
-           if(chckFirstDiaLr != "" && chckFirstDiaLr == chckSecndDiaLr && chckFirstDiaLr == chckThirdDiaLr){
-                alert(chckFirstDiaLr + "s WIN!!!!")
-           }
+         //functionality added to place if statements in function
+         //simplified variable usage by passing the getElementId calls//
+         //as arguments to the functions and removing variable names.
+           for(letterCnt = 0; letterCnt <= 2; letterCnt++){
+                theWinner(document.getElementById("0_" + letterCnt).innerText,/*was chckFirstSqrR*/
+                 document.getElementById("1_" + letterCnt).innerText,/*was chckSecndSqrR*/
+                 document.getElementById("2_" + letterCnt).innerText); /*was chckThirdSqrR*/
+     
+            //     if(chckFirstSqrR == " "){
+            //         continue;
+            //      }
+            //     // theWinner(chckFirstSqrR,chckSecndSqrR,chckThirdSqrR)
+        
+            //      }
+            // }     
             
-           var chckFirstDiaRl = document.getElementById("0_2").innerText;
-           var chckSecndDiaRl = document.getElementById("1_1").innerText; 
-           var chckThirdDiaRl = document.getElementById("2_0").innerText;
-           if(chckFirstDiaRl != "" && chckFirstDiaRl == chckSecndDiaRl && chckFirstDiaRl == chckThirdDiaRl){
-                alert(chckFirstDiaRl + "s WIN!!!!")
-           }
+         //functionality added to count through each column on board till 3 of same letters(innerText) found in the column     
+            // for(letterCnt = 0; letterCnt <= 2; letterCnt++){
+               theWinner(document.getElementById(letterCnt + "_0").innerText,/*was hckFirstSqrC*/
+                      document.getElementById(letterCnt + "_1").innerText,/*was chckSecndSqrC */ 
+                      document.getElementById(letterCnt + "_2").innerText);/*was chckThirdSqrC*/
+           } 
+       /* tried to use continue, break and return as read in w3 schools.//
+    //  Need to understand how they work better. This continue does not work to check column empty*/
+    //  if(chckFirstSqr == " ") continue;{}
+    //     }
+    //* make sure first box not empty before checking for winner- Doesn't work like expected*/
+    //  if(chckFirstSqr == " "){
+
+    //function to find 3 in a row by checking locating square,checking letter in square
+    //and comparing 3 squares to see if equal and announce winner 
+                // if(chckFirstSqrC == " "){
+                //          continue;
+                //      }
+                    //  theWinner(chckFirstSqrC,chckSecndSqrC,chckThirdSqrC)                  
+                //  }
+        
+              
+    //check letters diagnolly left to right then right to left for winner  
+            theWinner(document.getElementById("0_0").innerText,/*was chckFirstDiaL*/
+                      document.getElementById("1_1").innerText, /*was chckSecndDiaLr*/
+                      document.getElementById("2_2").innerText);/*was chckThirdDiaLr*/
+    
+            // if(chckFirstDiaLr != "" && chckFirstDiaLr == chckSecndDiaLr && chckFirstDiaLr == chckThirdDiaLr){
+            //      alert(chckFirstDiaLr + "DiagLr  WIN!!!!")
+            // }
+        
+            theWinner(document.getElementById("0_2").innerText,/*was chckFirstDiaRl*/
+                     document.getElementById("1_1").innerText,/*was chckSecndDiaRl*/ 
+                     document.getElementById("2_0").innerText);/*was chckThirdDiaRl*/
+            // if(chckFirstDiaLr != "" && chckFirstDiaRl == chckSecndDiaRl && chckFirstDiaRl == chckThirdDiaRl){
+            //      alert(chckFirstDiaRl + "DiagRl WIN!!!!")
+                 }
+            
+        
+
+        function theWinner(chckFirstSqr,chckSecndSqr,chckThirdSqr){
+            if(chckFirstSqr != " " && chckFirstSqr == chckSecndSqr && chckFirstSqr == chckThirdSqr){
+                /*Extra.  Instead of X wins, display player name and letter in parenthes "is Winner"*/
+                    alert(chckFirstSqr + "row WIN!!!!!");
+                    win = true;
+                }
         }
-
-
-
-
-
-
-
-//*EXTRA-window prompt ask for players names and says hello at beginning of game but does it more than once-->
+// *EXTRA-window prompt ask for players names and says hello at beginning of game but does it more than once-->
 // function plyrs () {
 // var plyr1 = window.prompt("Player 1-Please Enter your name: "); alert("Hello: " + plyr1);
 // var plyr2 = window.prompt("Player 2-Please Enter your name: "); alert("Hello: " + plyr2);
-// }
-// plyrs();
-//Testing ForEach******
-//input array of numbers
-//output array numbers greater than 
-///Test-Expect numbers GreterThan ten to equal 14,20,77
-var counter = 0
-var Array = []
-
-function NumsIn(testNumbers) {
-  testNumbers.forEach(number => {
-    if (number > 10) {
-      counter = number
-      Array.push(counter)
-    }
-    return 0
-  });
-  console.log(Array)
-}
-NumsIn([77, 7, 14, 3, 20])
+// 
